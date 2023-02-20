10aedd67cdfce4bd95066487f6b40a3338921ed2
Вячеслав <denisenko.ve21@gmail.com>
2023-02-20 11:37:14 +0300

    version #4

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.kata.spring.boot_security.demo.service;
 
+import org.hibernate.Hibernate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.Role;
@@ -21,6 +23,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void saveUser(User user) {
         Set<Role> roles = new HashSet<>();
         roles.add(roleDao.getOne(1L));
@@ -29,21 +32,27 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findByUsername(String username) {
-        return userDao.findByUsername(username);
+        User user = userDao.findByUsername(username);
+        Hibernate.initialize(user.getAuthorities());
+        return user;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User findById(Long id) {
         return userDao.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userDao.findAll();
     }
 
     @Override
+    @Transactional
     public void deleteById(Long id) {
         userDao.deleteById(id);
     }
